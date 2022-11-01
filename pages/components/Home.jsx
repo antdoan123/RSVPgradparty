4594ee7fbf8da7458dc285c149f07576009d51a6
antdoan123 <aqdoan@cpp.edu>
2022-11-01 02:55:39 -0700

    fixed rest of apos

--- pages/components/Home.jsx
+++ pages/components/Home.jsx
@@ -45,12 +45,12 @@ const Home = () => {
                 <img className="h-60" src="./pics.png"></img>
 
                 <div className='text-white px-5 py-4 max-w-md mt-6'>
-                    <h2 className='text-xl'><span className='font-bold text-blue-200'>Event: </span>Anthony's Grad Party</h2>
+                    <h2 className='text-xl'><span className='font-bold text-blue-200'>Event: </span>Anthony&apos;s Grad Party</h2>
                     <h2 className='text-xl'><span className='font-bold text-blue-200'>Date:  </span>Friday, Jan 20, 2023</h2>
                     <h2 className='text-xl'><span className='font-bold text-blue-200'>Time:  </span>6:00pm - 12:00am</h2>
                     <h2 className='text-xl'><span className='font-bold text-blue-200'>Location:  </span>Santa Fe Spring Social Halls</h2>
                     <h2 className='text-xl'><span className='font-bold text-blue-200'>Address:  </span><a href="https://maps.app.goo.gl/8fa62FjLY4vQhSMG9?g_st=ic" target="_blank" rel="noopener noreferrer" className='hover:underline hover:text-blue-400'>11710 Telegraph Rd, Santa Fe Springs, CA 90670</a></h2>
-                    <h2 className='text-xl'><span className='font-bold text-blue-200'>Description: </span>Celebrating Anthony's Graduation with a BS Computer Engineering degree <a href="#" className='hover:underline hover:text-blue-400'>@Cal Poly Pomona</a> w/ 3.79 GPA</h2>
+                    <h2 className='text-xl'><span className='font-bold text-blue-200'>Description: </span>Celebrating Anthony&apos;s Graduation with a BS Computer Engineering degree <a href="#" className='hover:underline hover:text-blue-400'>@Cal Poly Pomona</a> w/ 3.79 GPA</h2>
                     <div className="flex flex-nowrap mt-4 items-center justify-center px-12 md:px-24  text-gray-500">
 
                         <a href="https://forms.gle/iFPbMyJrSExeJGVW6" target="_blank" rel="noopener noreferrer" className="flex text-white w-fit px-20 py-3 my-2 rounded-2xl bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer hover:scale-105 duration-200">
